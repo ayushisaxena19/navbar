88b585446be15a25e8e60c7b7e4601c74de79358
unknown <ayushi1910093@akgec.ac.in>
2020-11-20 10:43:50 +0530

    initial commit

---
+++ tut53.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+
+// function greet(name,greetText="hello all"){
+//     console.log(greetText + " " + "to all" );
+//     console.log(name + " " + " is a good girl ");
+// }
+// function sum (a,b,c){
+//     let d= a+b+c;
+//     return d;
+// }
+// let name="ayushi";
+// let name1="riya";
+// let  name2="pari";
+// greetText="good morning";
+// greet(name,greetText);
+// greet(name1,greetText);
+
+// let returnVal=greet(name2);
+// console.log(returnVal);
+// let returnVal=sum(1,2,3);
+// console.log(returnVal);
+function greater(a,b){
+    let c=a-b;
+    return c;
+
+}
+let a=23;
+let b =45;
+let c= a-b;
+if(c>0)
+{console.log(c);
+    console.log(a +"is greater");
+
+}
+else{
+    console.log(b +"is greater");
+}
